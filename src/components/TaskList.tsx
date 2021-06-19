9dd54e0d1e222ab13cda71cd2c4cbee31fb3763e
Leandro Ferroni <ferroni.leandro@gmail.com>
2021-06-18 21:30:39 -0300

    Complete Task List actions

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle?.length > 0) {
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      };
+
+      setTasks(
+        [...tasks, newTask]
+      );
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(
+      tasks.map(item => { 
+        if (item.id === id) {
+          item.isComplete = !item.isComplete;
+        }
+        return item;
+      })
+    );
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(
+      tasks.filter(item => item.id !== id)
+    );
   }
 
   return (
